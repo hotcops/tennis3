9071475d737bb61d383032ac8ed0a51697ca641d
hotcops <shane.pizz@gmail.com>
2016-04-01 13:07:50 -0400

    Implemented ng-include for most of page. Slick-Carousel doesn't work w/ ng-include. Want to switch to directive instead.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,32 @@
-'use strict';
 
-/**
- * @ngdoc overview
- * @name tennisApp
- * @description
- * # tennisApp
- *
- * Main module of the application.
- */
-angular
-  .module('tennisApp', []);
+// Main Angular App
+angular.module('tennisApp', [])
+.controller('MainCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log('This is helloWorld() working in mainCtrl!');
+  };
+})
+
+/* NOT CURRENTLY WORKING
+.directive('intensityBanner', function() {
+  return {
+    controller: 'mainCtrl',
+    templateUrl: 'public/views/intensityBanner.html'
+  };
+});
+*/
+
+
+
+
+// Slick Plugin Settings
+$(document).ready(function(){
+      $('.slideshow').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        centerMode: true,
+        infinite: true,
+        variableWidth: true
+      });
+    });
