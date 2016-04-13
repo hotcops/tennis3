421e3ad34b19d77cec61af88115846e781d42808
Shane Pizzutello <shane.pizz@gmail.com>
2016-04-13 11:14:27 -0400

    Added mailer.php and code to app.js to handle form submission. It sends the email, but it loads another page after submit. Let's fix this so it stays on same page.

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,6 +1,6 @@
 (function($) {
     "use strict";
-	
+
 	// Options for Message
 	//----------------------------------------------
   var options = {
@@ -12,48 +12,11 @@
 	  'useAJAX': true,
   };
 
-	// Login Form
-	//----------------------------------------------
-	// Validation
-  $("#login-form").validate({
-  	rules: {
-      lg_username: "required",
-  	  lg_password: "required",
-    },
-  	errorClass: "form-invalid"
-  });
-  
-	// Form Submission
-  $("#login-form").submit(function() {
-  	remove_loading($(this));
-		
-		if(options['useAJAX'] == true)
-		{
-			// Dummy AJAX request (Replace this with your AJAX code)
-		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
-		
-		  // Cancel the normal submission.
-		  // If you don't want to use AJAX, remove this
-  	  return false;
-		}
-  });
-	
 	// Register Form
 	//----------------------------------------------
 	// Validation
   $("#register-form").validate({
   	rules: {
-      reg_username: "required",
-  	  reg_password: {
-  			required: true,
-  			minlength: 5
-  		},
-   		reg_password_confirm: {
-  			required: true,
-  			minlength: 5,
-  			equalTo: "#register-form [name=reg_password]"
-  		},
   		reg_email: {
   	    required: true,
   			email: true
@@ -74,13 +37,13 @@
   // Form Submission
   $("#register-form").submit(function() {
   	remove_loading($(this));
-		
+
 		if(options['useAJAX'] == true)
 		{
 			// Dummy AJAX request (Replace this with your AJAX code)
 		  // If you don't want to use AJAX, remove this
   	  dummy_submit_form($(this));
-		
+
 		  // Cancel the normal submission.
 		  // If you don't want to use AJAX, remove this
   	  return false;
@@ -96,17 +59,17 @@
     },
   	errorClass: "form-invalid"
   });
-  
+
 	// Form Submission
   $("#forgot-password-form").submit(function() {
   	remove_loading($(this));
-		
+
 		if(options['useAJAX'] == true)
 		{
 			// Dummy AJAX request (Replace this with your AJAX code)
 		  // If you don't want to use AJAX, remove this
   	  dummy_submit_form($(this));
-		
+
 		  // Cancel the normal submission.
 		  // If you don't want to use AJAX, remove this
   	  return false;
@@ -125,7 +88,7 @@
   {
     $form.find('[type=submit]').addClass('clicked').html(options['btn-loading']);
   }
-  
+
   function form_success($form)
   {
 	  $form.find('[type=submit]').addClass('success').html(options['btn-success']);
@@ -138,19 +101,4 @@
   	$form.find('.login-form-main-message').addClass('show error').html(options['msg-error']);
   }
 
-	// Dummy Submit Form (Remove this)
-	//----------------------------------------------
-	// This is just a dummy form submission. You should use your AJAX function or remove this function if you are not using AJAX.
-  function dummy_submit_form($form)
-  {
-  	if($form.valid())
-  	{
-  		form_loading($form);
-  		
-  		setTimeout(function() {
-  			form_success($form);
-  		}, 2000);
-  	}
-  }
-	
 })(jQuery);
